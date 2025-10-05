16a03ffad8a71c34b1254b1f1454057701aaac01
Edwin Ayala <edwin.e.ayala1997@gmail.com>
2025-10-04 21:26:45 -0600

    Leccion Functions

---
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -0,0 +1,40 @@
+
+
+
+
+function greet( name: string ): string{
+    return `Hola ${name}`
+}
+
+const message: string =  greet('Edwin');
+
+console.log( message );
+
+
+const greet2 = ( name: string ): string=>{
+    return `Hola ${ name }`
+};
+
+console.log( greet2( 'Enoc' ) );
+
+interface User{
+    uid: string;
+    username: string
+}
+
+const user = {
+        uid: 'ABC-123',
+        username: 'EDD-1456'
+    };
+
+function getUser( user: User ): User{
+    return user
+}
+
+console.log( getUser( user ) );
+
+const getUser2 = ( user: User ):User=>{
+    return user;
+};
+
+console.log( getUser2( user ) );
