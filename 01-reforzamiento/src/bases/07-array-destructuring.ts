5d05449857a0a303c2efcaf8e8568301971759e7
Edwin Ayala <edwin.e.ayala1997@gmail.com>
2025-10-06 22:48:26 -0600

    Leccion Desestructuracion de Objetos y Arreglos completadas

--- 01-reforzamiento/src/bases/07-array-destructuring.ts
+++ 01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -19,3 +19,38 @@ console.log( { trunks } );
 const returnsArray = (): (string | number)[]=>{
     return ['ABX', 123];
 };
+
+const [userName, userCode] = returnsArray();
+
+console.log( {userName, userCode} );
+
+
+//----------------------------------------------
+
+//TAREA
+
+/* La función debe llamarse useState.
+
+Debe retornar un arreglo con dos elementos:
+
+#1 - Un string (el valor inicial).
+
+#2 - Una función anónima de flecha que:
+
+Recibe un string.
+Imprime ese string en consola.
+ */
+
+const useState = ( initialValue: string ): any[]=>{
+
+    return [
+        initialValue,
+        ( value: string ): void=> console.log( value )
+    ] as const;
+};
+
+const [ name, setName ] = useState('Goku');
+
+console.log( name );
+
+setName( 'Dexter' );
---
+++ 01-reforzamiento/src/bases/data/heores.data.ts
@@ -0,0 +1,37 @@
+
+interface Hero{
+    id: number;
+    name: string;
+    owner: Owner
+}
+
+type Owner = 'DC' | 'Marvel';
+
+
+const heroes: Hero = [
+  {
+    id: 1,
+    name: 'Batman',
+    owner: 'DC',
+  },
+  {
+    id: 2,
+    name: 'Spiderman',
+    owner: 'Marvel',
+  },
+  {
+    id: 3,
+    name: 'Superman',
+    owner: 'DC',
+  },
+  {
+    id: 4,
+    name: 'Flash',
+    owner: 'DC',
+  },
+  {
+    id: 5,
+    name: 'Wolverine',
+    owner: 'Marvel',
+  },
+];
